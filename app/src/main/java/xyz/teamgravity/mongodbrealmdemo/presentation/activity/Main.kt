be2f56be0120ebf0053590e52065ffd245229b39
raheemadamboev <vividricemaryandmax@gmail.com>
2024-03-06 17:14:33 +0500

    Installed hilt;

--- app/src/main/java/xyz/teamgravity/mongodbrealmdemo/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/mongodbrealmdemo/presentation/activity/Main.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 import xyz.teamgravity.mongodbrealmdemo.presentation.theme.MongoDBRealmDemoTheme
 
+@AndroidEntryPoint
 class Main : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
